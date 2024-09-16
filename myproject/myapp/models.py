625bbd7e76070a4af1dfae3377d1c2a1b100e648
Laie <167744369+Lalaie@users.noreply.github.com>
2024-09-16 16:51:56 -0300

    Update models.py

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -23,6 +23,27 @@ class UserProfile(models.Model):
     birthday = models.DateField(null=True, blank=True)
     address = models.CharField(max_length=255, null=True, blank=True)
 
+class Tarjeta(models.Model):
+    nombre = models.CharField(max_length=200)
+    
+    def __str__(self) -> str:
+        return self.nombre
+    
+class MetodosPago(models.Model):
+    nombre = models.CharField(max_length=200)
+    tarjeta = models.ForeignKey(Tarjeta, on_delete=models.CASCADE, null=True, blank=True)
+    
+    def __str__(self):
+        return f'{self.nombre} - Tarjeta: {self.tarjeta.nombre}'
+    
+class Compra(models.Model):
+    cliente = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    forma_pago = models.ForeignKey(MetodosPago, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.nombre} - Cliente: {self.cliente.user} - Forma de pago: {self.forma_pago.nombre}'
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -34,4 +55,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
-
