e845bc45d4d37508813bd792bb8a81cb3d1d5330
Laie <167744369+Lalaie@users.noreply.github.com>
2024-09-16 16:52:23 -0300

    Update admin.py

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Categoria
 from .models import Juego
+from .models import Juego
+from .models import Tarjeta
+from .models import MetodosPago
+from .models import Compra
 
 # Register your models here.
 admin.site.register(Categoria)
 admin.site.register(Juego)
+admin.site.register(Tarjeta)
+admin.site.register(MetodosPago)
+admin.site.register(Compra)
