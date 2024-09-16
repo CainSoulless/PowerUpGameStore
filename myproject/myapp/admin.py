ed1920af92f7fbc9fd305c725efd958f19fcaefc
Laie <167744369+Lalaie@users.noreply.github.com>
2024-09-16 16:52:31 -0300

    Update admin.py

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Categoria
 from .models import Juego
-from .models import Juego
 from .models import Tarjeta
 from .models import MetodosPago
 from .models import Compra
