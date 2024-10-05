aa4352e1edfe8ec18cf2c7c73d3b562587607876
Rodrigo Hormazabal <rod.hormazabal@duocuc.cl>
2024-10-05 13:45:47 -0300

    [ADD] env file with var

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -195,6 +200,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'powerupgamestore@gmail.com'
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_PASSWORD = 'qzgy idrp dziv ygth'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'PowerUpGameStore <powerupgamestore@gmail.com>'
